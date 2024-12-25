ce64a89eb6d5a30176db8bc31ba1c8a4ceee1264
mhussainilyas <mhussainilyas7@gmail.com>
2024-12-25 11:39:39 -1200

    debugger-statemen done

---
+++ Debugger-statement.js
@@ -0,0 +1,25 @@
+// =======================
+//       debugger;
+// =======================
+// The debugger; statement in JavaScript is a built-in tool that pauses the execution of your script and triggers the debugging functionality in your browser or development environment.
+
+// ------------- How to Use debugger -------------
+
+// 1: Add the Statement
+for (let i = 0; i <= 10; i++) {
+    debugger; // Execution will pause here
+    console.log(i);
+}
+
+// 2: Run the Code in Debug Mode
+// Open the browser's developer tools.
+// In Chrome, press F12 or Ctrl + Shift + I.
+// Navigate to the "Sources" tab.
+
+// 3: Inspect Variables and State
+// When the browser encounters debugger;, it will pause script execution, allowing you to:
+// Examine the values of variables in the Scope panel.
+// Step through the code line by line.
+
+// 4: Resume Execution
+// To continue execution, press the play button in the debugging interface or use the keyboard shortcut (F8 in Chrome DevTools).
