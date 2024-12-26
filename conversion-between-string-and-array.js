2bad721d3ca008fe2962e13964817837764a9d5c
mhussainilyas <mhussainilyas7@gmail.com>
2024-12-25 15:58:26 -1200

    array & string conversion done

---
+++ conversion-between-string-and-array.js
@@ -0,0 +1,11 @@
+// ======================================
+//     Conversion b/w string & array
+// ======================================
+
+// -------- String to Array --------
+let str = "a quick brown fox jumps over the lazy dog.";
+console.log(str.split(""));
+
+// -------- Array to String --------
+let arr = ["Hussain", "Suleman", "Zaryab"];
+console.log(arr.join(" "));
